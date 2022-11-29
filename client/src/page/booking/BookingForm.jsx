b3175a3d44a67fa553261d4d63e4b38312edac8d
tuan <rickytuan1991@gmail.com>
2022-11-29 02:00:53 +0200

    add booking

--- client/src/page/booking/BookingForm.jsx
+++ client/src/page/booking/BookingForm.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import style from "./booking.module.css";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import {  TimePicker } from "@mui/x-date-pickers";
-
-
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import TextField from "@mui/material/TextField";
 export default function BookingForm() {
+  const [value, setValue] = React.useState(null);
 
   return (
     <div className={style["bg"]}>
@@ -13,7 +13,19 @@ export default function BookingForm() {
         <h3 className="font-extrabold text-4xl text-green-800 text-center mb-10">Järvi-Pohjanmaan hospital</h3>
         <form action="">
           <label>please select your date</label>
-          
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DatePicker
+              closeOnSelect={true}
+              label="Select date"
+              value={value}
+              onChange={(newValue) => {
+                console.log(newValue);
+                setValue(newValue);
+              }}
+              renderInput={(params) => <TextField {...params} />}
+              toolbarFormat="dd MM YY"
+            />
+          </LocalizationProvider>
         </form>
       </div>
     </div>
