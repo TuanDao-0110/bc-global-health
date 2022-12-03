92625aa7178da8a33935479915446d8661c7351d
tuan <rickytuan1991@gmail.com>
2022-12-03 12:11:00 +0200

    finnish register

--- client/src/service/userService.js
+++ client/src/service/userService.js
@@ -3,27 +3,37 @@ import { BASE_URL } from "../util/url.js";
 import axios from 'axios'
 import { getUserProfile } from "../redux/reducer/userReducer.jsx";
 import { setUpToken } from "./tokenService.js";
+import { closeLoadingService, openLoadingService } from "./loadingService.js";
 
-
-
-export const handleRegister = async (userInfo) => {
+export const handleRegister = async (userInfo, dispatch, navigate) => {
+    openLoadingService(dispatch)
     try {
-        const data = await axios.post({
+        const {data} = await axios({
+            method: 'post',
             url: `${BASE_URL}/register`,
             data: userInfo
         })
-        const { msg } = data
-        alert(`${msg}`)
+        if (data) {
+            const { msg } = data
+            alert(`${msg} please login`)
+        }
+        closeLoadingService(dispatch)
+        navigate('/login')
     } catch (error) {
+        closeLoadingService(dispatch)
+        alert('nickname or email have been used')
         console.log(error)
     }
+
+
 }
 
 export const handleLogin = async (userInfo, dispatch) => {
     try {
-        const { token, msg } = await axios.post({
+        const { token, msg } = await axios({
+            method: 'post',
             url: `${BASE_URL}/login`,
-            data: userInfo
+            data: userInfo,
         })
         setUpToken()
         alert(msg)
