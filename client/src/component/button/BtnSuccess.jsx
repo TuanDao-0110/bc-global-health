3d83be325280bc30860db1ed8fbfc2751b46e05e
tuan <rickytuan1991@gmail.com>
2022-12-03 22:39:35 +0200

    add profile check

--- client/src/component/button/BtnSuccess.jsx
+++ client/src/component/button/BtnSuccess.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function BtnSuccess(props) {
-  return <button>{props?.name}</button>;
+  return <button className="block w-full bg-green-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">{props?.message}</button>;
 }
--- client/src/page/portfolio/Portfolio.jsx
+++ client/src/page/portfolio/Portfolio.jsx
@@ -1,19 +1,66 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import BtnSubmit from "../../component/button/BtnSubmit";
+import BtnSuccess from "../../component/button/BtnSuccess";
 import { closeLoadingService, openLoadingService } from "../../service/loadingService";
+import { handleGetUserProfile } from "../../service/userService";
 
 export default function Portfolio() {
   // dispatch
   const dispatch = useDispatch();
-
+  // get user profile
   useEffect(() => {
-    openLoadingService(dispatch);
-    closeLoadingService(dispatch);
+    handleGetUserProfile(dispatch);
   }, []);
-
-
-  return <div>
-
+  const { userProfile } = useSelector((state) => state.userReducer);
+  useEffect(() => {
+    setMainUserProfile(userProfile);
+  }, [userProfile]);
+  // get user profile
+  // set temp state for user profile
+  const [mainUserProfile, setMainUserProfile] = useState();
+  const renderProfile = () => {
+    let render = [];
+    for (let i in mainUserProfile) {
+      if (i !== "bookingList" && i !== "__v") {
+        render.push(
+          <div key={i} className="flex gap-5 my-3 justify-around ">
+            <label htmlFor="" className="md:mt-0 text-gray-800 w-1/5 font-semibold text-xl mb-2 flex items-center">
+              {i}
+            </label>
+            <input
+              disabled
+              defaultValue={`${mainUserProfile[i]}`}
+              type={`${i}`}
+              className="rounded-md text-xl text-gray-600 w-3/5 border-2 p-3  border-gray-400 bg-gray-200"
+            />
+          </div>
+        );
+      }
+    }
+    return render;
+  };
+  const handleEdit =()=> { 
     
-  </div>;
+  }
+  return (
+    <div className="w-4/5 mx-auto">
+      <div className="rounded-lg bg-gray-100  w-full flex justify-center items-center  md:flex-row my-10 p-5 shadow-2xl">
+        <form
+          className="rounded px-4 w-4/5"
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          {renderProfile()}
+
+          <div className="flex gap-5 justify-center w-4/5 m-auto my-5">
+            <BtnSubmit message={"Save"}></BtnSubmit>
+            <BtnSuccess message={"Edit"}></BtnSuccess>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
